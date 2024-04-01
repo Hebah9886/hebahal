e1eced06b685615ddc89d84dd5c25072b8c032f1
Hebah9886 <hebahalshammakhi@gmail.com>
2024-04-01 07:17:35 +0400

    On branch main  Your branch is up to date with 'origin/main'. Changes to be committed:  deleted:    Event-Countdown Changes not staged for commit: 	modified:   src/views/createevent.vue

--- src/views/createevent.vue
+++ src/views/createevent.vue
@@ -1,34 +1,41 @@
 <template>
   <div>
-    <div class="heading-container">
-      <h2>Create New Event</h2>
-    </div>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <div class="heading-container">
+            <h2>Create New Event</h2>
+          </div>
 
-    <div class="form-container">
-      <form @submit.prevent="addEvent" class="event-form">
-        <div class="form-group">
-          <label for="eventName">Event Name</label>
-          <input type="text" v-model="eventName" id="eventName" class="form-control" required>
+          <div class="form-container">
+            <form @submit.prevent="addEvent" class="event-form">
+              <div class="form-group">
+                <label for="eventName">Event Name</label>
+                <input type="text" v-model="eventName" id="eventName" class="form-control" required>
+              </div>
+              <div class="form-group">
+                <label for="eventDescription">Description</label>
+                <textarea v-model="eventDescription" id="eventDescription" class="form-control" rows="3" required></textarea>
+              </div>
+              <div class="form-group">
+                <label for="eventCommencement">Commencement Date and Time</label>
+                <input type="datetime-local" v-model="eventCommencement" id="eventCommencement" class="form-control" required>
+              </div>
+              <div class="form-group">
+                <label for="eventPhoto">Photo</label>
+                <input type="file" @change="handlePhotoUpload" accept="image/*" id="eventPhoto" class="form-control-file" required>
+                <img v-if="eventPhoto" :src="eventPhoto" alt="Event Photo" style="max-width: 200px; margin-top: 10px;">
+              </div>
+              <button type="submit" class="btn btn-primary">Add Event</button>
+            </form>
+          </div>
         </div>
-        <div class="form-group">
-          <label for="eventDescription">Description</label>
-          <textarea v-model="eventDescription" id="eventDescription" class="form-control" rows="3" required></textarea>
+        <div class="col-lg-4 mt-4 mt-lg-0"> <!-- Move image column to the right on large screens -->
+          <div class="img">
+            <img width="400px" :src="Image" alt="Example Image">
+          </div>
         </div>
-        <div class="form-group">
-          <label for="eventCommencement">Commencement Date and Time</label>
-          <input type="datetime-local" v-model="eventCommencement" id="eventCommencement" class="form-control" required>
-        </div>
-        <div class="form-group">
-          <label for="eventPhoto">Photo</label>
-          <input type="file" @change="handlePhotoUpload" accept="image/*" id="eventPhoto" class="form-control-file" required>
-          <img v-if="eventPhoto" :src="eventPhoto" alt="Event Photo" style="max-width: 200px; margin-top: 10px;">
-        </div>
-        <button type="submit" @click="showToatSuccess()" class="btn btn-primary">Add Event</button>
-      </form>
-    </div>
-
-    <div class="img">
-      <img width="400px" :src="Image" alt="Example Image">
+      </div>
     </div>
   </div>
 </template>
@@ -48,12 +55,6 @@ export default {
     };
   },
   methods: {
-    showToatSuccess(){
-            toast.success('Wow success!',{
-                autoClose: 1000,
-            });   
-        },
-    
     addEvent() {
       // Validate form fields
       if (!this.eventName || !this.eventDescription || !this.eventCommencement) {
@@ -96,51 +97,17 @@ export default {
       };
       reader.readAsDataURL(file);
     }
-    
   }
 };
 </script>
 
 <style scoped>
-/* Add global styles here */
+/* No need for scoped styles, Bootstrap will handle them */
 
-.heading-container {
-  text-align: center;
-  margin-top: 20px;
-}
-
-.form-container {
-  max-width: 600px;
-  margin: 0 auto;
-}
-
-.img{
-  display: flex;
-  position: absolute;
- right:50ex;top: 16ex;}
-
-@media (max-width: 767px) {
-  .form-container {
-    padding: 0 15px;
-  }
-
+/* Additional media query for tablets */
+@media (max-width: 991px) {
   .img {
-    margin-top: 50ex;
-    right: auto;
-    position: absolute;
-  }
-}
-
-@media (min-width: 768px) {
-  .form-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-start;
-  }
-
-  .form-group {
-    flex: 1;
-    margin-right: 20px;
+    margin-top: 20px;
   }
 }
 </style>
